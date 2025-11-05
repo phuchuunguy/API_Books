4faab4709c022d31a992d398504e7c54dae90551
Phuc <nguyhuuphuc@gmail.com>
2025-11-05 11:40:45 +0700

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,24 +7,28 @@ import { Category } from './categories/entity/category.entity';
 import { UsersModule } from './users/modules/users.module';
 import { AuthModule } from './auth/modules/auth.module';
 import { User } from './users/entity/user.entity';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.DB_HOST || 'Localhost',
-    port:parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USER || 'admin',
-    password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'booksdb',
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     entities: [Book, Category, User],
-    synchronize: true,
     }),
     BooksModule,
     CategoriesModule,
     UsersModule,
     AuthModule,
   ],
+  controllers: [AppController],
+  providers: [AppService], 
 }) 
 
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to the Bookstore API!';
+  getRoot(): any {
+    return {
+      message: 'Welcome to the Bookstore API! 🚀',
+      docs: 'api/docs',
+      books: '/books',
+      categories: '/categories',
+      users: '/users',
+      auth: '/auth',
+    };
   }
 }
